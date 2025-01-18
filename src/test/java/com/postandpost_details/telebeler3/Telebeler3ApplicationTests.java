3e5b12412e469564d4d27677f01169b1d3535577
Emrah1998 <emrahqarawov240@gmail.com>
2025-01-18 10:34:46 +0400

    Telebeler3

---
+++ src/test/java/com/postandpost_details/telebeler3/Telebeler3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.postandpost_details.telebeler3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Telebeler3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
